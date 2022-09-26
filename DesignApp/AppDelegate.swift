b72b5d1323a7252b4381d2c87df0808b6fd6df8b
AhmedEltrass <52069792+ahmedeltrass@users.noreply.github.com>
2022-09-26 14:20:39 +0200

    ahmed

--- DesignApp/AppDelegate.swift
+++ DesignApp/AppDelegate.swift
@@ -1,11 +1,12 @@
 //
 //  AppDelegate.swift
 //  DesignApp
-//
+//  
 //  Created by Ahmed Eltrass on 11/09/2022.
 //
 
 import UIKit
+//import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+    //   FirebaseApp.configure()
         return true
     }
 
